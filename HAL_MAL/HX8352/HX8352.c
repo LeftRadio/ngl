aa52bfcce5ceea09b9ad559ade347aa021e9df9d
Vladislav Kamenev <vladislav@inorbit.com>
2018-02-15 23:37:39 +0200

    HAL_MAL/HX8352/hx8352.c: fix X_Max, Y_Max and SetRotation/SetCursor/SetArea funcs

--- HAL_MAL/HX8352/HX8352.c
+++ HAL_MAL/HX8352/HX8352.c
@@ -1,15 +1,16 @@
 /**
   ******************************************************************************
-  * @file		     HX8352.c
-  * @author		Neil Lab :: Left Radio
-  * @version	v1.0.0
-  * @date		2015-03-10-22.27
-  * @brief		HAL sourse for HX8352 LCD controller
+  * @file         HX8352.c
+  * @author    Neil Lab :: Left Radio
+  * @version  v1.0.0
+  * @date    2015-03-10-22.27
+  * @brief    HAL sourse for HX8352 LCD controller
   ******************************************************************************
 **/
 
 /* Includes ------------------------------------------------------------------*/
 #include "NGL_types.h"
+#include "LCD_GPIO.h"
 #include "LCD_HAL.h"
 
 /* Private typedef -----------------------------------------------------------*/
@@ -30,8 +31,8 @@ NGL_HAL_Typedef HX8352 =
     LCD_GPIO_Connect,  // помним что у нас GPIO
     8,                             // 8-ми битная шина данных
     18,                           // цвет 18 бит на точку
-    399,                         // максимум по Х для "родной" ореинтации ЖК
-    239,                         // максимум по Y для "родной" ореинтации ЖК
+    239,                         // максимум по Х для "родной" ореинтации ЖК
+    399,                         // максимум по Y для "родной" ореинтации ЖК
     96000,                      // количество точек (X_Max + 1) * (Y_Max + 1) = 240 * 400 = 96000
     0x06,                    // регистр установки курсора по X
     0x02,                     // регистр установки курсора по Y
@@ -96,8 +97,11 @@ void HX8352_Init(void)
     NGL_HAL_WriteRegister(0x48,0x4F);
     NGL_HAL_WriteRegister(0x49,0x4F);
     //--------------------------------------------------------------------------------
+    /* color 16 bit
+       [6]CSEL2=1, [5]CSEL1=0, [4]CSEL0=1, [2]IFPF2=1, [1]IFPF1=0, [0]IFPF1 */
+    NGL_HAL_WriteRegister(0x17,0x55);
+
     // Power Supply Setting
-    NGL_HAL_WriteRegister(0x17,0x61); // RADJ=0110(85%), OSC_EN=1
     NGL_HAL_WriteRegister(0x2B,0xF9); // N_DCDC=0xF9.
     NGL_Delay(10);
     NGL_HAL_WriteRegister(0x1B,0x14); // BT=0001, AP=100
@@ -124,7 +128,7 @@ void HX8352_Init(void)
     NGL_HAL_WriteRegister(0x24,0x38); // GON=1, DTE=1, D=10
     NGL_Delay(40);
     NGL_HAL_WriteRegister(0x24,0x3C); // GON=1, DTE=1, D=11
-    NGL_HAL_WriteRegister(0x16,0x1C); // BGR=0
+    NGL_HAL_WriteRegister(0x16,0x38); // MX=0, MY=0, MV=1, ML=1, BGR=1
     NGL_HAL_WriteRegister(0x01,0x06); // INVON=0, NORNO=1
     NGL_HAL_WriteRegister(0x55,0x00);
     //--------------------------------------------------------------------------------
@@ -137,7 +141,7 @@ void HX8352_Init(void)
     NGL_HAL_WriteRegister(0x08,0x01); // Row address end2
     NGL_HAL_WriteRegister(0x09,0x8F); // Row address end1
 
-    HX8352.WriteCommand(0x22);
+    HX8352_WriteRAM_Prepare();
 
     CS_LCD_clr;
 
@@ -158,23 +162,31 @@ void HX8352_SetRotation(NGL_RotationLCD NewState)
     switch(NewState)
     {
     case _0_degree:
-
-
+        /* [7]MY=0, [6]MX=0, [5]MV=0, [4]ML=1, [3]BGR=1 */
+        NGL_HAL_WriteRegister(0x16, 0x18);
+        HX8352.X_Register = 0x02;
+        HX8352.Y_Register = 0x06;
         break;
 
     case _90_degree:
-
-
+        /* [7]MY=1, [6]MX=0, [5]MV=1, [4]ML=1, [3]BGR=1 */
+        NGL_HAL_WriteRegister(0x16, 0xB8);
+        HX8352.X_Register = 0x06;
+        HX8352.Y_Register = 0x02;
         break;
 
     case _180_degree:
-
-
+        /* [7]MY=1, [6]MX=1, [5]MV=0, [4]ML=1, [3]BGR=1 */
+        NGL_HAL_WriteRegister(0x16, 0xD8);
+        HX8352.X_Register = 0x02;
+        HX8352.Y_Register = 0x06;
         break;
 
     case _270_degree:
-
-
+        /* [7]MY=0, [6]MX=1, [5]MV=1, [4]ML=1, [3]BGR=1 */
+        NGL_HAL_WriteRegister(0x16, 0x78);
+        HX8352.X_Register = 0x06;
+        HX8352.Y_Register = 0x02;
         break;
 
     default:
@@ -190,19 +202,18 @@ void HX8352_SetRotation(NGL_RotationLCD NewState)
   * @param  uint16_t X, Y
   * @retval None
   */
-void HX8352_SetCursor(uint16_t X, uint16_t Y)
-{
-    CS_LCD_set;
+void HX8352_SetCursor(uint16_t X, uint16_t Y) {
+    uint16_t x = X, y = Y;
+    if((HX8352.Rotation == _90_degree) || (HX8352.Rotation == _270_degree)) {
+      x = Y, y = X;
+    }
+    NGL_HAL_WriteRegister(HX8352.X_Register, (uint8_t)(x >> 8)); /* Row */
+    NGL_HAL_WriteRegister(HX8352.X_Register + 1, (uint8_t)x); /* Row */
 
-    NGL_HAL_WriteRegister(HX8352.X_Register, X >> 8); /* Row */
-    NGL_HAL_WriteRegister(HX8352.X_Register + 1, (uint8_t)X); /* Row */
+    NGL_HAL_WriteRegister(HX8352.Y_Register, (uint8_t)(y >> 8)); /* Line */
+    NGL_HAL_WriteRegister(HX8352.Y_Register + 1, (uint8_t)y); /* Line */
 
-    NGL_HAL_WriteRegister(HX8352.Y_Register, Y >> 8); /* Line */
-    NGL_HAL_WriteRegister(HX8352.Y_Register + 1, (uint8_t)Y); /* Line */
-
-    HX8352.WriteCommand(0x0022);
-
-    CS_LCD_clr;
+    HX8352_WriteRAM_Prepare();
 }
 
 
@@ -211,22 +222,29 @@ void HX8352_SetCursor(uint16_t X, uint16_t Y)
   * @param  uint16_t X0, Y0, X1, Y1
   * @retval None
   */
-void HX8352_SetArea(uint16_t X0, uint16_t Y0, uint16_t X1, uint16_t Y1)
-{
-	CS_LCD_set;
+void HX8352_SetArea(uint16_t X0, uint16_t Y0, uint16_t X1, uint16_t Y1) {
 
-	NGL_HAL_WriteRegister(0x02,Y0 >> 8); // Column address start2
-	NGL_HAL_WriteRegister(0x03,Y0);   // Column address start1
-	NGL_HAL_WriteRegister(0x04,Y1 >> 8); // Column address end2
-	NGL_HAL_WriteRegister(0x05,Y1);   // Column address end1
-	NGL_HAL_WriteRegister(0x06,X0 >> 8); // Row address start2
-	NGL_HAL_WriteRegister(0x07,X0); // Row address start1
-	NGL_HAL_WriteRegister(0x08,X1 >> 8); // Row address end2
-	NGL_HAL_WriteRegister(0x09,X1); // Row address end1
+  uint16_t x0 = X0, y0 = Y0, x1 = X1, y1 = Y1;
 
-	HX8352.WriteCommand(0x22);
+  CS_LCD_set;
 
-	CS_LCD_clr;
+  if((HX8352.Rotation == _90_degree) || (HX8352.Rotation == _270_degree)) {
+    x0 = Y0, y0 = X0, x1 = Y1, y1 = X1;
+  }
+
+  NGL_HAL_WriteRegister(HX8352.X_Register, x0 >> 8); // Row address start2
+  NGL_HAL_WriteRegister(HX8352.X_Register+1, x0); // Row address start1
+  NGL_HAL_WriteRegister(HX8352.X_Register+2, x1 >> 8); // Row address end2
+  NGL_HAL_WriteRegister(HX8352.X_Register+3, x1); // Row address end1
+
+  NGL_HAL_WriteRegister(HX8352.Y_Register, y0 >> 8); // Column address start2
+  NGL_HAL_WriteRegister(HX8352.Y_Register+1, y0);   // Column address start1
+  NGL_HAL_WriteRegister(HX8352.Y_Register+2, y1 >> 8); // Column address end2
+  NGL_HAL_WriteRegister(HX8352.Y_Register+3, y1);   // Column address end1
+
+  HX8352_WriteRAM_Prepare();
+
+  CS_LCD_clr;
 }
 
 
